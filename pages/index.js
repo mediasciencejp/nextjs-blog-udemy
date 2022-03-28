17ecdc32078993aa4462e61ed7e546d55f25bf45
mediasciencejp <contact@mediascience.jp>
2022-03-29 02:00:43 +0900

    profile changed

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>私はインチキのにしやんです。インチキポスターを販売しています。</p>
+        <p>私はインチキのにしやんです。インチキポスターを販売して逮捕されました。</p>
       </section>
 
       <section>
